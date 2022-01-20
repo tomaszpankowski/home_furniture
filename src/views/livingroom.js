3f2e423c104016abf1434a58fb62562d6d61b570
tomaszpankowski <pankowski.email@gmail.com>
2022-01-20 19:39:50 +0100

    modified routing

---
+++ src/views/livingroom.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+import Container from "../../node_modules/react-bootstrap/Container";
+
+class Livingroom extends Component{
+    render(){
+        return(        
+            <Container fluid className="livingroom-s1 d-flex align-items-center minh-100vh">
+                
+            </Container>    
+        );
+    }
+}
+
+export default Livingroom;
